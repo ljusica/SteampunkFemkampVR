45abfe808f509f91c7734df060f66a18b3cf8259
NeoLjusica <jessiicaceder@hotmail.com>
2022-09-09 09:40:25 +0200

    duck

--- SteampunkFemkampVR/Assets/Script/DuckHunt/DuckMovement.cs
+++ SteampunkFemkampVR/Assets/Script/DuckHunt/DuckMovement.cs
@@ -6,11 +6,28 @@ public class DuckMovement : MonoBehaviour
 {
     void Start()
     {
-        
+        /*
+         * Put all tier children in array
+         * 
+         * foreach var duck in array
+         * move x random in range
+         * if endpoint gets reached -> move y
+         * after timer expires -> move y
+         */
     }
 
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+        /*
+         * if other gametag == duck
+         * change x direction the other way
+         */
+
     }
 }
