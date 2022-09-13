712cf0ef976a54ad06d2da95e96c0b8511e1b3df
NeoLjusica <jessiicaceder@hotmail.com>
2022-09-13 11:45:13 +0200

    ducks b floating

--- Assets/Script/DuckHunt/Floater.cs
+++ Assets/Script/DuckHunt/Floater.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class Floater : MonoBehaviour
 {
-    public Rigidbody rb;
+    public GameObject[] ducks;
+    private float[] ducksX;
+
+    public WaveManager waveManager;
+
     public float depthBeforeSubmerged = 1;
     public float displacementAmount = 3;
 
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        ducksX = new float[ducks.Length];
+        for(int i = 0; i < ducks.Length; i++)
+        {
+            ducksX[i] = ducks[i].transform.position.x;
+        }
+    }
+
     private void FixedUpdate()
     {
-        float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x);
-
-        if(transform.position.y < waveHeight)
+        for(int i = 0; i < ducks.Length; i++)
         {
-            float displacementMultiplier = Mathf.Clamp01((waveHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
-            rb.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), ForceMode.Acceleration);
+            rb = ducks[i].GetComponent<Rigidbody>();
+            float waveHeight = waveManager.GetWaveHeight(ducksX[i]);
+
+            if (ducks[i].transform.position.y < waveHeight)
+            {
+                float displacementMultiplier = Mathf.Clamp01((waveHeight - ducks[i].transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+                rb.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), ForceMode.Acceleration);
+            }
         }
+
     }
 
 }
