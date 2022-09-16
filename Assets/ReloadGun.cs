958b2662a8d20e3f24ede82291d97f6a164412fe
MackanMackan <38558616+MackanMackan@users.noreply.github.com>
2022-09-16 11:15:44 +0200

    fix gun ver 5011

--- Assets/ReloadGun.cs
+++ Assets/ReloadGun.cs
@@ -4,31 +4,13 @@ using UnityEngine;
 
 public class ReloadGun : MonoBehaviour
 {
-    [SerializeField]
-    GameObject lever;
-    Vector3 startPos;
-    Rigidbody rb;
-    void Start()
-    {
-        startPos = lever.transform.localPosition;
-        rb = GetComponent<Rigidbody>();
-    }
-
-    public void OnReleaseLever()
-    {
-        Mathf.Lerp(transform.localPosition.z, startPos.z,1);
-        rb.constraints = RigidbodyConstraints.FreezePositionZ;
-    }
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
 
-        if (collision.gameObject.CompareTag("ReloadLever"))
+        if (collision.CompareTag("ReloadLever"))
         {
             FireGun.canShoot = true;
         }
     }
-    public void OnGrabLever()
-    {
-        rb.constraints &= ~RigidbodyConstraints.FreezePositionZ;
-    }
+
 }
