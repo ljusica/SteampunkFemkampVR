ded9b1db117724db9f0abcc1d9190d2b00c37f1a
MackanMackan <38558616+MackanMackan@users.noreply.github.com>
2022-09-16 12:39:06 +0200

    updated gun

--- Assets/ReloadGun.cs
+++ Assets/ReloadGun.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class ReloadGun : MonoBehaviour
 {
+
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnReleaseLever()
+    {
+        rb.isKinematic = true;
+    }
     private void OnTriggerEnter(Collider collision)
     {
 
         if (collision.CompareTag("ReloadLever"))
         {
             FireGun.canShoot = true;
+            Debug.Log("Reload");
         }
     }
-
+    public void OnGrabLever()
+    {
+        rb.isKinematic = false;
+    }
 }
