5ddbf4a58244f75f36c4754131a2986fe6991d53
MackanMackan <38558616+MackanMackan@users.noreply.github.com>
2022-09-16 14:16:29 +0200

    gun finlir

--- Assets/ReloadGun.cs
+++ Assets/ReloadGun.cs
@@ -1,31 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ReloadGun : MonoBehaviour
 {
+    [SerializeField]
+    XRGrabInteractable interactable;
 
-    Rigidbody rb;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
 
-    public void OnReleaseLever()
-    {
-        rb.isKinematic = true;
-    }
     private void OnTriggerEnter(Collider collision)
     {
 
         if (collision.CompareTag("ReloadLever"))
         {
             FireGun.canShoot = true;
-            Debug.Log("Reload");
+            interactable.enabled = false;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("ReloadLever"))
+        {
+            FireGun.canShoot = true;
+            interactable.enabled = true;
         }
     }
     public void OnGrabLever()
     {
-        rb.isKinematic = false;
+        
     }
 }
