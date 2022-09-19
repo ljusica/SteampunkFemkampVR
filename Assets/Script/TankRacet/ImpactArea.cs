31be8a038fa5bb2de7b9fd3ab90b49084b0157f1
NeoLjusica <jessiicaceder@hotmail.com>
2022-09-19 11:41:54 +0200

    Nice and very cool moving cube for TR

--- Assets/Script/TankRacet/ImpactArea.cs
+++ Assets/Script/TankRacet/ImpactArea.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ImpactArea : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float duration = 3;
+   
+    public float minYPos;
+    public float maxYPos;
+    public float minXPos;
+    public float maxXPos;
+    
+    private float endPointY;
+    private float endPointX;
+
     void Start()
     {
-        
+        Move(minXPos, maxXPos, minYPos, maxYPos);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Move(float minX, float maxX, float minY, float maxY)
     {
-        
+        endPointX = Random.Range(minX, maxX);
+        endPointY = Random.Range(minY, maxY);
+
+        Vector3 endPoint = new Vector3(endPointX, endPointY, transform.position.z);
+
+        transform.DOMove(endPoint, duration).SetEase(Ease.OutBounce).OnComplete(() => Move(minXPos, maxXPos, minYPos, maxYPos));
     }
 }
