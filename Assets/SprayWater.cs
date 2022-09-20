5d4979762498bc2d069d5c4896d66c33195f5891
MackanMackan <38558616+MackanMackan@users.noreply.github.com>
2022-09-20 15:00:56 +0200

    fixe water gin null ref

--- Assets/SprayWater.cs
+++ Assets/SprayWater.cs
@@ -10,18 +10,26 @@ public class SprayWater : MonoBehaviour
 
     TestControlls controller;
     InputAction shoot;
+    Vector3 forwardPoint;
     public void StartSpraying()
     {
         InvokeRepeating(nameof(Spray),0,0.05f);
     }
     public void Spray()
     {
-        Physics.Raycast(transform.position,transform.forward,out RaycastHit hit, 30);
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 30))
+        {
+            forwardPoint = transform.forward * 30;
+        }
+        else
+        {
+            forwardPoint = hit.transform.position;
+        }
         lineRenderer.SetPosition(0, new Vector3(transform.position.x, transform.position.y, transform.position.z));
-        lineRenderer.SetPosition(1, hit.point);
+        lineRenderer.SetPosition(1, forwardPoint);
         lineRenderer.enabled = true;
         waterHitParticle.gameObject.SetActive(true);
-        waterHitParticle.transform.position = hit.point;
+        waterHitParticle.transform.position = forwardPoint;
     }
     public void ReleaseSpray()
     {
