6bd75bc30faab791c2f73a0f92f75ac3b5a9028d
Carl Christensen <carl.christensen98@gmail.com>
2022-09-20 13:47:57 +0200

    Changed level loading to async loading

--- Assets/Script/MenuScripts/SelectLevel.cs
+++ Assets/Script/MenuScripts/SelectLevel.cs
@@ -1,13 +1,29 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class SelectLevel : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        //if(other.gameObject.tag == "Ball")
+        //{
+        //    SceneManager.LoadScene(other.gameObject.GetComponent<LevelName>()?.levelName);
+        //}
+
         if(other.gameObject.tag == "Ball")
         {
-            SceneManager.LoadScene(other.gameObject.GetComponent<LevelName>()?.levelName);
+            StartCoroutine("LoadSceneAsync", other.gameObject.GetComponent<LevelName>()?.levelName);
+        }
+    }
+
+    private IEnumerator LoadSceneAsync(string levelName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelName);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
         }
     }
 }
