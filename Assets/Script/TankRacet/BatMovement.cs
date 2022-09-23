87cdea1ace7978e7f66f9ec8b9b5e76238631bb6
NeoLjusica <jessiicaceder@hotmail.com>
2022-09-23 14:07:24 +0200

    Updates to TankRacet-scene

--- Assets/Script/TankRacet/BatMovement.cs
+++ Assets/Script/TankRacet/BatMovement.cs
@@ -8,28 +8,15 @@ public class BatMovement : MonoBehaviour
     public Transform startPoint;
     public Transform endPoint;
 
-    public bool finished = false;
+    [HideInInspector] public bool finished = false;
     public delegate void BatFinish();
     public static BatFinish batFinish;
 
-    private float score;
     private bool moving;
 
     private void Start()
     {
         gameObject.transform.position = startPoint.transform.position;
-    }
-
-    void Update()
-    {
-        //score = ScoreManager.Instance.GetScore("TankRacet");
-
-        //gameObject.transform.DOMoveX(endPoint.transform.position.x, score * Time.deltaTime);
-
-        //if(gameObject.transform.position.x >= endPoint.transform.position.x)
-        //{
-        //    finished = true;
-        //}
     }
 
     public void Move()
