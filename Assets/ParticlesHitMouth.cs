54d3cd5b5fdbb02cb597a6851a215d19041442c7
MackanMackan <38558616+MackanMackan@users.noreply.github.com>
2022-09-26 11:44:15 +0200

    added blood gun to TR scene

--- Assets/ParticlesHitMouth.cs
+++ Assets/ParticlesHitMouth.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class ParticlesHitMouth : MonoBehaviour
 {
     public BatMovement batMovement;
-    public ParticleSystem ps;
     private void OnParticleCollision(GameObject other)
     {
         batMovement.Move();
