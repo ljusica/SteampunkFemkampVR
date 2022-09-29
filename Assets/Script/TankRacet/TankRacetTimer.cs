77fa79943fe15624477886addbd388bfc1215247
NeoLjusica <jessiicaceder@hotmail.com>
2022-09-29 11:48:09 +0200

    Added vampire head + started working on new hands

--- Assets/Script/TankRacet/TankRacetTimer.cs
+++ Assets/Script/TankRacet/TankRacetTimer.cs
@@ -1,6 +1,7 @@
 using System;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 public class TankRacetTimer : MonoBehaviour
 {
@@ -8,7 +9,11 @@ public class TankRacetTimer : MonoBehaviour
     public TextMeshPro scoreText;
     public TextMeshPro timerText;
     public GameObject objectsToActivate;
+    public GameObject jaw;
+    public ImpactArea vampire;
 
+    private Vector3 jawDown;
+    private Vector3 jawUp;
     private float countDown;
     private BatMovement batMovement;
     private bool countingDown;
@@ -17,6 +22,8 @@ public class TankRacetTimer : MonoBehaviour
     {
         BatMovement.batFinish += Score;
         batMovement = GameObject.FindGameObjectWithTag("Bat").GetComponent<BatMovement>();
+        jawUp = jaw.transform.localPosition;
+        jawDown = new Vector3(jawUp.x, jawUp.y - 0.6f, jawUp.z);
     }
 
     private void Score()
@@ -38,8 +45,12 @@ public class TankRacetTimer : MonoBehaviour
 
     public void StartCountDown()
     {
+        vampire.moving = true;
         batMovement.gameObject.transform.position = batMovement.startPoint.transform.position;
         batMovement.finished = false;
+        
+        jaw.transform.DOLocalMove(jawDown, 1).SetEase(Ease.OutBounce).OnComplete(() => vampire.Move()); 
+        
         countDown = timer;
         countingDown = true;
         ScoreManager.Instance.ResetScore();
@@ -49,6 +60,8 @@ public class TankRacetTimer : MonoBehaviour
     void CountDownEnd()
     {
         countingDown = false;
+        vampire.moving = false;
+        jaw.transform.DOLocalMove(jawUp, 1);
         objectsToActivate.SetActive(false);
         timerText.text = "Time: " + 0;
     }
