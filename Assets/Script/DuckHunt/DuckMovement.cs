82635126cae28f070ef8868b91758dae34de94ae
NeoLjusica <jessiicaceder@hotmail.com>
2022-09-30 11:14:17 +0200

    Changes to score and balancing in SkullHunt

--- Assets/Script/DuckHunt/DuckMovement.cs
+++ Assets/Script/DuckHunt/DuckMovement.cs
@@ -5,13 +5,22 @@ using DG.Tweening;
 
 public class DuckMovement : MonoBehaviour
 {
-    public GameObject[] ducks;
+    public GameObject[] ducksTier1;
+    public GameObject[] ducksTier2;
+    public GameObject[] ducksTier3;
 
     public float farLeft;
     public float farRight;
     public float hidingPos;
+    public float speedTier1;
+    public float speedTier2;
+    public float speedTier3;
 
     private float startPosY;
+    private float startYTier1;
+    private float startYTier2;
+    private float startYTier3;
+    
     private float endPoint;
     private float timer;
     private float counter = 5;
@@ -22,38 +31,36 @@ public class DuckMovement : MonoBehaviour
     {
         timer = Time.time;
 
-        for (int i = 0; i < ducks.Length; i++)
-        {
-            startPosY = ducks[i].transform.localPosition.y;
-            hidingPos = startPosY - 2f;
-
-            Move(ducks[i]);
-        }
-
+        startYTier1 = CalculateStartingPosition(ducksTier1, speedTier1);
+        startYTier2 = CalculateStartingPosition(ducksTier2, speedTier2);
+        startYTier3 = CalculateStartingPosition(ducksTier3, speedTier3);
     }
 
     private void Update()
     {
         if (Time.time >= timer + counter)
         {
-            for (int i = 0; i < ducks.Length; i++)
-            {
-                hidingPropability = Random.Range(0, 2);
-
-                if (hidingPropability > 0)
-                {
-                    Hide(ducks[i]);
-                }
-                else
-                {
-                    ducks[i].transform.DOLocalMoveY(startPosY, 1);
-                }
-            }
+            CalculateHide(ducksTier1, 2, startYTier1);
+            CalculateHide(ducksTier2, 4, startYTier2);
+            CalculateHide(ducksTier3, 5, startYTier3);
 
             counter = Random.Range(2, 7);
             timer = Time.time;
         }
 
+    }
+
+    private float CalculateStartingPosition(GameObject[] ducks, float speed)
+    {
+        for (int i = 0; i < ducks.Length; i++)
+        {
+            startPosY = ducks[i].transform.localPosition.y;
+            hidingPos = startPosY - 2f;
+
+            Move(ducks[i], speed);
+        }
+
+        return startPosY;
     }
 
     private float ChooseNewDestination()
@@ -62,11 +69,28 @@ public class DuckMovement : MonoBehaviour
         return endPoint;
     }
 
-    private void Move(GameObject duck)
+    private void Move(GameObject duck, float speed)
     {
         float duckDestination = ChooseNewDestination();
 
-        duck.transform.DOMoveX(duckDestination, 10 * Mathf.Abs(duck.transform.position.x - duckDestination)).OnComplete(() => Move(duck));
+        duck.transform.DOMoveX(duckDestination, speed * Mathf.Abs(duck.transform.position.x - duckDestination)).OnComplete(() => Move(duck, speed));
+    }
+
+    private void CalculateHide(GameObject[] ducks, int maxChance, float startPositionY)
+    {
+        for (int i = 0; i < ducks.Length; i++)
+        {
+            hidingPropability = Random.Range(0, maxChance);
+
+            if (hidingPropability > 0)
+            {
+                Hide(ducks[i]);
+            }
+            else
+            {
+                ducks[i].transform.DOLocalMoveY(startPositionY, 1);
+            }
+        }
     }
 
     public void Hide(GameObject duck)
