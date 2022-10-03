de6b296a1cbae59bd0b4135c729e116a0cc56dd4
MackanMackan <38558616+MackanMackan@users.noreply.github.com>
2022-10-03 11:21:03 +0200

    added clown laugh in eyeball

--- Assets/Script/SkeeBall/SkeeballScoreAudioHandler.cs
+++ Assets/Script/SkeeBall/SkeeballScoreAudioHandler.cs
@@ -6,11 +6,14 @@ public class SkeeballScoreAudioHandler : MonoBehaviour
 {
     [SerializeField]
     AudioSource audioSource;
+    [SerializeField]
+    List<AudioClip> clownLaughClips;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
+            audioSource.clip = clownLaughClips[Random.Range(0, clownLaughClips.Count)];
             audioSource.Play();
         }
     }
