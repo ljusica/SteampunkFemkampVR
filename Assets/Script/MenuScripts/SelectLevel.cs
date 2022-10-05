f734f6204d93e1355783889f9e69bb0f61bf09e1
Carl Christensen <carl.christensen98@gmail.com>
2022-10-05 13:55:16 +0200

    Changed some level select codes

--- Assets/Script/MenuScripts/SelectLevel.cs
+++ Assets/Script/MenuScripts/SelectLevel.cs
@@ -28,7 +28,6 @@ public class SelectLevel : MonoBehaviour
 
     public void LoadScene(string levelName)
     {
-        Debug.Log("Mamma mia är en film som finns, det är inget skämt");
         SceneManager.LoadSceneAsync(levelName);
         animator.SetTrigger("FadeOut");
     }
