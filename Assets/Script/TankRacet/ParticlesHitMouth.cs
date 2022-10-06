0ad40d91b672b11e1294b6af460ec8f8b5f5bb59
MackanMackan <38558616+MackanMackan@users.noreply.github.com>
2022-10-06 11:22:21 +0200

    added decal in orject. and added decals spawn when hit vamipre in bloodlust

--- Assets/Script/TankRacet/ParticlesHitMouth.cs
+++ Assets/Script/TankRacet/ParticlesHitMouth.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class ParticlesHitMouth : MonoBehaviour
 {
     public BatMovement batMovement;
     public ParticleSystem ps;
     public AudioSource hitMouthSound;
+    public GameObject bloodDecal;
+    public List<Material> bloodMaterials;
+    public GameObject vampire;
+
     List<ParticleSystem.Particle> particles;
+    bool bloodSplattered = false;
     private void Start()
     {
         particles = new List<ParticleSystem.Particle>();
@@ -21,7 +27,21 @@ public class ParticlesHitMouth : MonoBehaviour
             {
                 hitMouthSound.pitch += 0.2f;
                 hitMouthSound.Play();
+                if (!bloodSplattered)
+                {
+                    SpawnBloodDecal();
+                }
             }
         }
+    }
+    IEnumerator SpawnBloodDecal()
+    {
+        bloodSplattered = true;
+        GameObject decal = Instantiate(bloodDecal);
+        decal.transform.position = particles[0].position;
+        decal.transform.parent = vampire.transform;
+        decal.GetComponent<DecalProjector>().material = bloodMaterials[Random.Range(0, bloodMaterials.Count)];
+        yield return new WaitForSeconds(0.5f);
+        bloodSplattered = false;
     }
 }
